55420a9abfea9a27669ef0a7d4295ce2e74a2ae4
ゆっくりたく <77869346+Yukkuritaku@users.noreply.github.com>
2021-01-28 11:33:24 +0900

    Add files via upload

---
+++ net/yukkuritaku/commands/stamp/impl/niigo/EnaStampCommand.java
@@ -0,0 +1,79 @@
+package net.yukkuritaku.commands.stamp.impl.niigo;
+
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.yukkuritaku.utils.ReplyUtils;
+
+import java.util.List;
+import java.util.Random;
+
+public final class EnaStampCommand extends Command {
+
+
+    public EnaStampCommand(){
+        this.name = "stampena";
+        this.help = "えななんのスタンプからどれかが選ばれるよ";
+        this.aliases = new String[] {"enanan", "ena", "えな", "えなちゃん", "えななん", "東雲絵名", "東雲絵名ちゃん"};
+    }
+
+    @Override
+    public void execute(CommandEvent event){
+        try {
+            final List<RandomEnanan> Values = List.of(RandomEnanan.values());
+            final int Size = Values.size();
+            final Random rng = new Random();
+            final RandomEnanan enanan = Values.get(rng.nextInt(Size));
+            EmbedBuilder embedBuilder = new EmbedBuilder();
+            embedBuilder.setImage("https://sekai-res.dnaroma.eu/file/sekai-assets/stamp/" + enanan + "_rip/" + enanan + "/" + enanan + ".webp");
+            embedBuilder.setColor(0xCCAA88);
+            ReplyUtils.logInfo(event, "Sending...");
+            event.reply(embedBuilder.build());
+            ReplyUtils.logSuccess(event, "Successful!");
+        }
+        catch (Exception exception){
+            ReplyUtils.logError(event, "Send Failed!");
+            ReplyUtils.logError(event, "Please Contact to ゆっくりたく(Yukkuritaku)#8298");
+            exception.printStackTrace();
+        }
+    }
+
+    public enum RandomEnanan{
+        //(自撮り)
+        stamp0220,
+
+        //ねむい...
+        stamp0219,
+
+        //またね～
+        stamp0218,
+
+        //...ごめんってば
+        stamp0217,
+
+        //ありがとう！
+        stamp0216,
+
+        //よかったね♪
+        stamp0215,
+
+        //うん...
+        stamp0214,
+
+        //えへへ
+        stamp0213,
+
+        //きゃっ！
+        stamp0212,
+
+        //ほどほどにがんばろ？
+        stamp0211,
+
+        //よろしく♪
+        stamp0210,
+
+        //いいねついてる♪
+        stamp0030,
+
+    }
+}
---
+++ net/yukkuritaku/commands/stamp/impl/niigo/MahuyuStampCommand.java
@@ -0,0 +1,81 @@
+package net.yukkuritaku.commands.stamp.impl.niigo;
+
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.yukkuritaku.utils.ReplyUtils;
+
+import java.util.List;
+import java.util.Random;
+
+//もしかしたらMahuyuじゃなくてMafuyuの方が正しい？
+public final class MahuyuStampCommand extends Command {
+
+    public MahuyuStampCommand(){
+        this.name = "stampmahuyu";
+        this.help = "まふゆちゃんのスタンプからどれかが選ばれるよ";
+        this.aliases = new String[] {"mahuyu", "mhy", "mafuyu", "mfy", "まふゆちゃん", "まふゆ", "よくわからない", "yokuwakaranai", "朝比奈まふゆ", "朝比奈まふゆちゃん", "雪"};
+    }
+
+    @Override
+    protected void execute(CommandEvent event) {
+        try{
+            final List<RandomMahuyuStamps> Values = List.of(RandomMahuyuStamps.values());
+            final int Size = Values.size();
+            final Random rng = new Random();
+            final RandomMahuyuStamps mahuyuStamps = Values.get(rng.nextInt(Size));
+            EmbedBuilder embedBuilder = new EmbedBuilder();
+            embedBuilder.setImage("https://sekai-res.dnaroma.eu/file/sekai-assets/stamp/" + mahuyuStamps + "_rip/" + mahuyuStamps + "/" + mahuyuStamps + ".webp");
+            embedBuilder.setColor(0x8888CC);
+            ReplyUtils.logInfo(event, "Sending...");
+            event.reply(embedBuilder.build());
+            ReplyUtils.logSuccess(event, "Successful!");
+        }
+        catch (Exception e){
+            ReplyUtils.logError(event, "Send Failed!");
+            ReplyUtils.logError(event, "Please Contact to ゆっくりたく(Yukkuritaku)#8298");
+            e.printStackTrace();
+        }
+    }
+
+    public enum RandomMahuyuStamps{
+        //よくわからない
+        stamp0422,
+
+        //ひとりになりたい...(裏の表情、虚無の顔)
+        stamp0416,
+
+        //...(裏の表情、虚無の顔)
+        stamp0415,
+
+        //またね！
+        stamp0209,
+
+        //ごめんなさい
+        stamp0208,
+
+        //ありがとう
+        stamp0207,
+
+        //よかったね！
+        stamp0206,
+
+        //あ...
+        stamp0205,
+
+        //うれしいな
+        stamp0204,
+
+        //きゃっ！
+        stamp0203,
+
+        //いっしょにがんばろうね！
+        stamp0202,
+
+        //よろしくね
+        stamp0201,
+
+        //いい子、ね...
+        stamp0029,
+    }
+}
